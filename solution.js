120cd8c8c42152ad272bc6b2632507a62a7cb9e4
BorisDmitriev <113904873+BorisDmitriev@users.noreply.github.com>
2023-10-09 20:49:11 +0200

    Initial commit

---
+++ solution.js
@@ -0,0 +1,47 @@
+// 0
+let apples = 20;
+let oranges = 30;
+
+//1
+console.log("1:");
+console.log(apples == oranges);
+
+//2
+console.log("2:");
+console.log(apples != oranges);
+
+//3
+console.log("3:");
+console.log(apples > oranges);
+
+//4
+console.log("4:");
+console.log(apples <= oranges);
+
+//5
+console.log("5:");
+console.log(oranges > apples);
+
+//6
+console.log("6:");
+let mangoes = 5;
+console.log( (apples-mangoes) < (oranges/mangoes) );
+
+//7
+console.log("7:");
+console.log( (mangoes*apples) > (mangoes + oranges) );
+
+//8
+console.log("8:");
+console.log( mangoes==oranges? mangoes==oranges : false );
+console.log( oranges==apples? oranges==apples : false );
+
+//9
+console.log("9:");
+console.log( (apples%mangoes) === oranges%mangoes);
+
+//10
+console.log("10:");
+console.log( (mangoes+apples) > (oranges-mangoes), (mangoes+apples) === (oranges-mangoes) );
+
+
